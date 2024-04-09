3673038a698ea07f4170628a4c9316a1fc6e2208
joaopjalles <104013326+joaopjalles@users.noreply.github.com>
2024-04-09 11:02:48 -0300

    Add files via upload

---
+++ projeto/src/Calculadora.java
@@ -0,0 +1,14 @@
+public class Calculadora {
+    int soma(int a, int b) {
+        return a + b;
+    }
+
+    double soma(double a, double b) {
+        return a + b;
+    }
+
+    int soma(int a, int b, int c) {
+        return a + b + c;
+    }
+
+}
